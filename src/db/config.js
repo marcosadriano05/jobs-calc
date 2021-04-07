cf02a768cd9c57bbd85ebb7b53a0adf042656c83
Marcos Adriano <marcosadriano740@gmail.com>
2021-04-07 17:42:17 -0300

    feat: init database

--- src/db/config.js
+++ src/db/config.js
@@ -1,11 +1,11 @@
 const sqlite3 = require('sqlite3');
 const { open } = require('sqlite');
 
-module.exports = () => {
+module.exports = () => (
   open(
     {
       filename: './database.sqlite',
       driver: sqlite3.Database
     }
-  );
-};
+  )
+);
